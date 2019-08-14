b181314756710631d39f1e60dd7a305524bb4271
cardoso <matheus@cardo.so>
2019-08-14 10:52:29 -0300

    Added benchmarking

--- E3Kit iOS Swift Sample/ViewController.swift
+++ E3Kit iOS Swift Sample/ViewController.swift
@@ -20,14 +20,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         log = { [weak self] text in
             DispatchQueue.main.async {
+                print(text)
                 self?.logsTextView.text += "\(text)\n"
             }
         }
 
+        log("* Testing main methods:");
+        log("\n----- EThree.initialize -----");
         initializeUsers {
+            log("\n----- EThree.register -----");
             self.registerUsers {
+                log("\n----- EThree.lookupPublicKeys -----");
                 self.lookupPublicKeys {
                     do {
+                        log("\n----- EThree.encrypt & EThree.decrypt -----");
                         try self.encryptAndDecrypt()
                     } catch(let e) {
                         log(e)
@@ -44,18 +50,20 @@ class ViewController: UIViewController {
     var aliceLookup: EThree.LookupResult?
 
     func initializeUsers(_ completion: @escaping Completion) {
-        log("Initializing Alice")
         alice.initialize { error in
             if let error = error {
-                log("Failed initializing Alice: \(error)")
+                log("Failed initializing \(self.alice.identity): \(error)")
                 return
+            } else {
+                log("Initialized \(self.alice.identity)")
             }
 
-            log("Initializing Bob")
             self.bob.initialize { error in
                 if let error = error {
-                    log("Failed initializing Bob: \(error)")
+                    log("Failed initializing \(self.bob.identity): \(error)")
                     return
+                } else {
+                    log("Initialized \(self.bob.identity)")
                 }
 
                 completion()
@@ -64,18 +72,20 @@ class ViewController: UIViewController {
     }
 
     func registerUsers(_ completion: @escaping Completion) {
-        log("Registering Alice")
         alice.register { error in
             if let error = error {
-                log("Failed registering Alice: \(error)")
+                log("Failed registering \(self.alice.identity): \(error)")
                 return
+            } else {
+                log("Registered \(self.alice.identity)")
             }
 
-            log("Registering Bob")
             self.bob.register { error in
                 if let error = error {
-                    log("Failed registering Bob: \(error)")
+                    log("Failed registering \(self.bob.identity): \(error)")
                     return
+                } else {
+                    log("Registered \(self.bob.identity)")
                 }
 
                 completion()
@@ -85,21 +95,21 @@ class ViewController: UIViewController {
 
 
     func lookupPublicKeys(_ completion: @escaping Completion) {
-        log("Looking up Bob's public key")
-        alice.lookupPublicKeys(of: ["Bob"]) {
+        alice.lookupPublicKeys(of: [bob.identity]) {
             switch $0 {
             case .failure(let error):
-                log("Failed looking up Bob's public key: \(error)")
+                log("Failed looking up \(self.bob.identity)'s public key: \(error)")
             case .success(let lookup):
+                log("Looked up \(self.bob.identity)'s public key")
                 self.bobLookup = lookup
             }
 
-            log("Looking up Alice's public key")
-            self.bob.lookupPublicKeys(of: ["Alice"]) {
+            self.bob.lookupPublicKeys(of: [self.alice.identity]) {
                 switch $0 {
                 case .failure(let error):
-                    log("Failed looking up Alice's public key: \(error)")
+                    log("Failed looking up \(self.alice.identity)'s public key: \(error)")
                 case .success(let lookup):
+                    log("Looked up \(self.alice.identity)'s public key")
                     self.aliceLookup = lookup
                     completion()
                 }
@@ -108,15 +118,33 @@ class ViewController: UIViewController {
     }
 
     func encryptAndDecrypt() throws {
-        let aliceEncryptedText = try alice.encrypt(text: "Hello Bob!", for: bobLookup)
-        log("Alice encrypts and signs: '\(aliceEncryptedText)'")
-        let aliceDecryptedText = try bob.decrypt(text: aliceEncryptedText, from: aliceLookup!["Alice"])
-        log("Bob decrypts and verifies Alice's signature: '\(aliceDecryptedText)'")
+        var time0 = timeInMs()
+        let aliceEncryptedText = try alice.encrypt(text: "Hello \(bob.identity)!", for: bobLookup)
+        var time1 = timeInMs()
 
-        let bobEncryptedText = try bob.encrypt(text: "Hello Alice!", for: aliceLookup)
-        log("Bob encrypts and signs: '\(bobEncryptedText)'")
-        let bobDecryptedText = try alice.decrypt(text: bobEncryptedText, from: bobLookup!["Bob"])
-        log("Alice decrypts and verifies Bob's signature: '\(bobDecryptedText)'")
+        log("\(alice.identity) encrypts and signs: '\(aliceEncryptedText)'. Took: \(time1 - time0)ms")
+
+        time0 = timeInMs()
+        let aliceDecryptedText = try bob.decrypt(text: aliceEncryptedText, from: aliceLookup![alice.identity])
+        time1 = timeInMs()
+
+        log("\(bob.identity) decrypts and verifies \(alice.identity)'s signature: '\(aliceDecryptedText)'. Took: \(time1 - time0)ms")
+
+        time0 = timeInMs()
+        let bobEncryptedText = try bob.encrypt(text: "Hello \(alice.identity)!", for: aliceLookup)
+        time1 = timeInMs()
+
+        log("\(bob.identity) encrypts and signs: '\(bobEncryptedText)'. Took: \(time1 - time0)ms")
+
+        time0 = timeInMs()
+        let bobDecryptedText = try alice.decrypt(text: bobEncryptedText, from: bobLookup![bob.identity])
+        time1 = timeInMs()
+
+        log("\(alice.identity) decrypts and verifies \(bob.identity)'s signature: '\(bobDecryptedText)'. Took: \(time1 - time0)ms")
+    }
+
+    func timeInMs() -> Double {
+        return Double(DispatchTime.now().rawValue)/1000000
     }
 
 }
